878192906daa71dab8739ca70964efebb19fc80d
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-03-10 13:42:53 +0100

    initial logic implementation for snack machine operations

--- SnaclMachne.Logic/SnackMachine.cs
+++ SnaclMachne.Logic/SnackMachine.cs
@@ -4,5 +4,63 @@ namespace SnackMachine.Logic
 {
     public sealed class SnackMachine
     {
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set; }
+        public int QuaterCount { get; private set; }
+        public int OneDollarCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
+
+        //transaction Amounts
+        public int OneCentCountInTransacton { get; private set; }
+        public int TenCentCountInTransacton { get; private set; }
+        public int QuaterCountInTransacton { get; private set; }
+        public int OneDollarCountInTransacton { get; private set; }
+        public int FiveDollarCountInTransacton { get; private set; }
+        public int TwentyDollarCountInTransacton { get; private set; }
+
+        public void InsertMoney(int oneCentCount,
+            int tenCentCount, 
+            int quaterCount, 
+            int oneDollarCount, 
+            int fiveDollarCount, 
+            int twentyDollarCount)
+        {
+
+            OneCentCountInTransacton += oneCentCount;
+            TenCentCountInTransacton += tenCentCount;
+            QuaterCountInTransacton += quaterCount;
+            OneDollarCountInTransacton += oneDollarCount;
+            FiveDollarCountInTransacton += fiveDollarCount;
+            TwentyDollarCountInTransacton += twentyDollarCount;
+        }
+
+        public void ReturnMoney()
+        {
+            OneCentCountInTransacton = 0;
+            TenCentCountInTransacton =0;
+            QuaterCountInTransacton =0;
+            OneDollarCountInTransacton = 0;
+            FiveDollarCountInTransacton =0;
+            TwentyDollarCountInTransacton = 0;
+        }
+
+        public void BuySnack()
+        {
+            OneCentCount += OneCentCountInTransacton;
+            TenCentCount += TenCentCountInTransacton;
+            QuaterCount += QuaterCountInTransacton;
+            OneDollarCount += OneDollarCountInTransacton;
+            FiveDollarCount += FiveDollarCountInTransacton;
+            TwentyDollarCount += TwentyDollarCountInTransacton;
+
+            OneCentCountInTransacton = 0;
+            TenCentCountInTransacton = 0;
+            QuaterCountInTransacton = 0;
+            OneDollarCountInTransacton = 0;
+            FiveDollarCountInTransacton = 0;
+            TwentyDollarCountInTransacton = 0;
+        }
+
     }
 }
