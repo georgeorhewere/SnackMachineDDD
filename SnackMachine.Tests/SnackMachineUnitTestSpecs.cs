c2524b77b5b9da3291090bfd63a8ea10dafe4892
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-03-11 20:42:23 +0100

    add test to confirm inserted note or coin goes to moneyInTransaction

--- SnackMachine.Tests/SnackMachineUnitTestSpecs.cs
+++ SnackMachine.Tests/SnackMachineUnitTestSpecs.cs
@@ -10,12 +10,28 @@ namespace SnackMachine.Tests
         [Fact]
         public void return_money_empties_money_in_transaction()
         {
+            //Arrange
+            var snackMachine = new SnackMachine.Logic.SnackMachine();            
+            snackMachine.InsertMoney(Money.OneDollar);
+            
+            //Act
+            snackMachine.ReturnMoney();
+            //Assert
+            Assert.Equal(0m,snackMachine.MoneyInTransaction.Amount);
+        }
+
+        [Fact]
+        public void inserted_money_goes_to_money_in_transaction()
+        {
+            //Arrange
             var snackMachine = new SnackMachine.Logic.SnackMachine();
+            
+            //Act
+            snackMachine.InsertMoney(Money.OneCent);
             snackMachine.InsertMoney(Money.OneDollar);
 
-            snackMachine.ReturnMoney();
-
-            Assert.Equal(0m,snackMachine.MoneyInTransaction.Amount);
+            //Assert
+            Assert.Equal(1.01m, snackMachine.MoneyInTransaction.Amount);
 
         }
 
