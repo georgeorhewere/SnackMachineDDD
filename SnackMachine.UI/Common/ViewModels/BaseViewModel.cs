ded1a86ff12a0fe12dc05fb4dfa8471de5598b1c
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-05-24 20:52:13 +0100

    Added Base viewmodel for the snack machine

---
+++ SnackMachine.UI/Common/ViewModels/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackMachine.UI.Common.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        protected void Notify(string property)
+        {
+            OnPropertyChanged(property);
+        }
+
+
+    }
+}
