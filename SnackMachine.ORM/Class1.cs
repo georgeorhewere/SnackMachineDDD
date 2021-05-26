7490085baa964344316dca6eb4f02f655472e9c2
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-05-26 17:15:05 +0100

    updated project type for ORM

---
+++ SnackMachine.ORM/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SnackMachine.ORM
+{
+    public class Class1
+    {
+    }
+}
---
+++ SnackMachine.ORM/SnackMachineDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnackMachine.ORM
+{
+    public class SnackMachineDbContext : DbContext
+    {
+        private readonly string connectionString = "Data Source=Hp/Druidsdba;initial catalog=SnackMachineDb;User ID=storeuser;Password=storeuser1; Connect Timeout=120;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(connectionString);
+        }
+    }
+}
